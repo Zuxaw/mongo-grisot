466abfcb475263027be09b33dd07a40715b214f4
Lisa_Duterte <lisa.duterte@hotmail.fr>
2022-12-14 17:13:24 +0100

    update replication script

--- rs-init.sh
+++ rs-init.sh
@@ -6,19 +6,19 @@ var config = {
     "version": 1,
     "members": [
         {
-            "_id": 1,
+            "_id": 0,
             "host": "mongo1:27017",
             "priority": 3
         },
         {
-            "_id": 2,
+            "_id": 1,
             "host": "mongo2:27017",
-            "priority": 2
+            "priority": 1
         },
         {
-            "_id": 3,
+            "_id": 2,
             "host": "mongo3:27017",
-            "priority": 1
+            "priority": 2
         }
     ]
 };
