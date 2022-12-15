679ec5eeba0c5f63b6fa78fdd9ac56e42dc1ee75
Maxime <50484560+Deffohn@users.noreply.github.com>
2022-12-15 13:24:41 +0100

    Update generate_metadata.py

--- generate_metadata.py
+++ generate_metadata.py
@@ -2,14 +2,23 @@ import random
 import time
 from time import sleep
 import requests
+
+#############
+# constants #
 url = 'http://172.16.232.229:5001/api/metadata/create'
 cooldown_loop_interval = 0.4  # seconds
+random_range = 3.2
+#############
+
+########################
+# simulation variables #
 planes = {"Airbus12345": (162.9, 4018.2), "Airbus14346": (191.2, 3029.3)}
+########################
 
 def createAndPostData():
     for i in planes.keys():
-        planes[i] = (planes[i][0] + random.uniform(-3.2, 3.2), planes[i][1] + random.uniform(-3.2, 3.2))
-        print(planes[i][1], requests.post(url, json={
+        planes[i] = (planes[i][0] + random.uniform(-random_range, random_range), planes[i][1] + random.uniform(-random_range, random_range))
+        print(requests.post(url, json={
             "timestamp": str(int(time.time())),
             "latitude": planes[i][0],
             "longitude": planes[i][1]
